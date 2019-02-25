d9517239db326802944204b563376a6a8f89817f
Guysmow <41370451+Guysmow@users.noreply.github.com>
2019-02-25 12:33:49 +0100

    Thanks Dokta

--- apex-api/src/Client.js
+++ apex-api/src/Client.js
@@ -20,32 +20,24 @@ class Client {
    * @param {string} platform The platform of the player to be searched
    */
   user(username, platform) {
-    return new Promise(async(resolve, reject) => {
-      if (!username) { return reject(new Error('[APEX-API] You must supply a username')); }
-      if (!platform) { return reject(new Error('[APEX-API] You must provide a platform [PC | XBOX | PSN]')); }
-      if (typeof username !== 'string') { return reject(new TypeError(`[APEX-API] Username expects a string, ${typeof username} given`)); }
-      if (typeof platform !== 'string') { return reject(new TypeError(`[APEX-API] Platform expects a string, ${typeof platform} given`)); }
-        let nbr;
-        if (platform === 'PC') { nbr = 5; }
-        if (platform === 'XBOX') { nbr = 1; }
-        if (platform === 'PSN') { nbr = 2; }
-        if(nbr == undefined) {          return reject(new TypeError(`[APEX-API] Platforms must be PC, XBOX or PSN`));        }
-      
-        const result = await fetch(`${URL}/${nbr}/${username}`, this.headers);
-        const data = await result.json();
-
-        // Invalid API Key
-        if (data.message === 'Invalid authentication credentials') { reject(new Error(`[APEX-API] Invalid API-Key`)); }
-
-        // Handling Player Not Found error
-        if (data.errors[0].message == 'Account not found. Please try again.') {
-          return reject(new TypeError(`[APEX-API] Account not found`));        }
-        // Handling any other error
-        else if (data.error) { return reject(data); }
-
-
-        resolve(data);
-
+    return new Promise((resolve, reject) => {
+      if (!username) return reject(new Error('[APEX-API] You must supply a username'));
+      if (!platform) return reject(new Error('[APEX-API] You must provide a platform [PC | XBOX | PSN]')); 
+      if (typeof username !== 'string') return reject(new TypeError(`[APEX-API] Username expects a string, ${typeof username} given`)); 
+      if (typeof platform !== 'string') return reject(new TypeError(`[APEX-API] Platform expects a string, ${typeof platform} given`));
+      let nbr;
+      if (platform.toUpperCase() === 'PC') nbr = 5;
+      if (platform.toUpperCase() === 'XBOX') nbr = 1; 
+      if (platform.toUpperCase() === 'PSN') nbr = 2; 
+      if(nbr == undefined) return reject(new TypeError(`[APEX-API] Platforms must be PC, XBOX or PSN`));      
+        fetch(`${URL}/${nbr}/${username}`, this.headers)
+          .then(res => res.json()) // FORMATING TO JSON
+          .then(response =>{
+            resolve(response) // REPONSE
+          }).catch(error =>{
+            reject(error.toString); // REJECT ERROR 
+            // A améliorer ! 
+          });
     });
   }
 };
